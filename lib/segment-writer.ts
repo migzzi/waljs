4573f852005cef976f917e5397b79f18d80f1a99
maged <m.magdy@awaed.co>
2024-11-11 20:13:55 +0200

    feat(segment-writer): Add segment writer implementation and tests.

---
+++ lib/segment-writer.ts
@@ -0,0 +1,105 @@
+import { WriteStream } from "fs";
+
+// The SegmentWriter is responsible for writing WAL entry records to disk.
+// This type handles the necessary buffered I/O as well as file system syncing.
+//
+// Every Entry is written, using the following binary layout (big endian format):
+//
+//	  ┌─────────────┬───────────┬──────────┬─────────┐
+//	  │ Offset (4B) │ Type (1B) │ CRC (4B) │ Payload │
+//	  └─────────────┴───────────┴──────────┴─────────┘
+//
+//		- Offset = 32bit WAL entry number for each record in order to implement a low-water mark
+//		- Type = Type of WAL entry
+//		- CRC = 32bit hash computed over the payload using CRC
+//		- Payload = The actual WAL entry payload data
+export class SegmentWriter {
+  private _size: number = 0;
+  get size(): number {
+    return this._size;
+  }
+
+  private currBatchSize: number = 0;
+  private currBatchIndex: number = 0;
+
+  private stream: WriteStream;
+
+  private corked = 0;
+
+  constructor(
+    writer: WriteStream,
+    private opts?: {
+      // The maximum size of a single segment file. Once this size is reached, a new
+      // segment file is created.
+      maxSegmentSize: number;
+      // Buffer size for writing WAL entries. This is used to batch writes to the
+      // underlying writer.
+      bufferSize: number;
+    }
+  ) {
+    this.opts = opts || { maxSegmentSize: 4 * 1024, bufferSize: 4 * 1024 };
+    this.stream = writer;
+  }
+
+  // Write a new WAL entry.
+  //
+  // Note, that we do not use the Entry interface here because encoding the
+  // payload is done at an earlier stage than actually writing data to the WAL
+  // segment.
+  async write(
+    offset: number,
+    typ: number,
+    checksum: number,
+    payload: Buffer
+  ): Promise<void> {
+    const header = Buffer.alloc(9);
+
+    header.writeUInt32BE(offset, 0);
+    header.writeUInt8(typ, 4);
+    header.writeUInt32BE(checksum, 5);
+
+    this.stream.cork();
+    this.corked++;
+
+    // If the current batch size exceeds the maximum allowed size, we need to
+    if (this.currBatchSize + 9 + payload.length > this.opts.bufferSize) {
+      await this.sync();
+
+      this.currBatchSize = 0;
+      this.currBatchIndex++;
+    }
+
+    this.stream.write(Buffer.concat([header, payload]));
+
+    this.currBatchSize += 9 + payload.length;
+    this._size += 9 + payload.length;
+  }
+
+  // Sync writes any buffered data to the underlying io.Writer and syncs the file
+  // systems in-memory copy of recently written data to disk if we are writing to
+  // an os.File.
+  sync(): Promise<void> {
+    if (this.corked === 0) {
+      return;
+    }
+
+    return new Promise((resolve, reject) => {
+      const corked = this.corked;
+      this.corked = 0;
+
+      process.nextTick(() => {
+        for (let i = 0; i < corked; i++) {
+          this.stream.uncork();
+        }
+        setImmediate(resolve);
+      });
+    });
+  }
+
+  // Close ensures that all buffered data is flushed to disk before and then closes
+  // the associated writer or file.
+  async close(): Promise<void> {
+    await this.sync();
+    this.stream.close();
+  }
+}
---
+++ tests/segment-writer.test.ts
@@ -0,0 +1,186 @@
+import { readFileSync, statSync, unlinkSync, writeFileSync } from "fs";
+import path from "path";
+import { SegmentWriter } from "../lib/segment-writer";
+
+import { open } from "fs/promises";
+import { createHash } from "crypto";
+
+describe("Test segment writer ops", () => {
+  const files: string[] = [];
+
+  afterAll(() => {
+    for (const file of files) {
+      unlinkSync(file);
+    }
+  });
+
+  it("Should write single line to segment", async () => {
+    const randomName = Math.random().toString(36).substring(7);
+    const filePath = path.join(__dirname, `${randomName}.wal`);
+    files.push(filePath);
+    const file = await open(filePath, "a+");
+    const writer = new SegmentWriter(file.createWriteStream());
+    const offset = 0;
+    const typ = 0;
+    const checksum = 0;
+    const payload = Buffer.from("test");
+
+    await writer.write(offset, typ, checksum, payload);
+
+    await writer.sync();
+
+    const content = readFileSync(filePath);
+    const expected = Buffer.concat([
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      payload,
+    ]);
+
+    expect(content).toEqual(expected);
+
+    await writer.close();
+  });
+
+  it("Should write multiple lines to segment", async () => {
+    const randomName = Math.random().toString(36).substring(7);
+    const filePath = path.join(__dirname, `${randomName}.wal`);
+    files.push(filePath);
+    const file = await open(filePath, "a+");
+    const writer = new SegmentWriter(file.createWriteStream());
+    const offset = 0;
+    const typ = 0;
+    const checksum = 0;
+    const payload1 = Buffer.from("test1");
+    const payload2 = Buffer.from("test2");
+    const payload3 = Buffer.from("test4");
+
+    await writer.write(offset, typ, checksum, payload1);
+    await writer.write(offset + 1, typ, checksum, payload2);
+    await writer.write(offset + 2, typ, checksum, payload3);
+
+    await writer.sync();
+
+    const content = readFileSync(filePath);
+    const expected = Buffer.concat([
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      payload1,
+      Buffer.from([0, 0, 0, 1, 0, 0, 0, 0, 0]),
+      payload2,
+      Buffer.from([0, 0, 0, 2, 0, 0, 0, 0, 0]),
+      payload3,
+    ]);
+
+    expect(content).toEqual(expected);
+  });
+
+  it("Test correct file size", async () => {
+    const randomName = Math.random().toString(36).substring(7);
+    const filePath = path.join(__dirname, `${randomName}.wal`);
+    files.push(filePath);
+    const file = await open(filePath, "a+");
+    const writer = new SegmentWriter(file.createWriteStream());
+    const offset = 0;
+    const typ = 0;
+    const checksum = 0;
+    const payload1 = Buffer.from("test1");
+    const payload2 = Buffer.from("test2");
+    const payload3 = Buffer.from("test4");
+
+    await writer.write(offset, typ, checksum, payload1);
+    await writer.write(offset + 1, typ, checksum, payload2);
+    await writer.write(offset + 2, typ, checksum, payload3);
+
+    await writer.sync();
+
+    expect(writer.size).toBe(
+      9 + payload1.length + 9 + payload2.length + 9 + payload3.length
+    );
+
+    const stat = statSync(filePath);
+    expect(stat.size).toBe(writer.size);
+  });
+
+  it("Test close - should flush", async () => {
+    const randomName = Math.random().toString(36).substring(10);
+    const filePath = path.join(__dirname, `${randomName}.wal`);
+    files.push(filePath);
+    const file = await open(filePath, "a+");
+    const writer = new SegmentWriter(file.createWriteStream());
+    const offset = 0;
+    const typ = 0;
+    const checksum = 0;
+    const payload1 = Buffer.from("test1");
+    const payload2 = Buffer.from("test2");
+    const payload3 = Buffer.from("test4");
+
+    await writer.write(offset, typ, checksum, payload1);
+    await writer.write(offset + 1, typ, checksum, payload2);
+    await writer.write(offset + 2, typ, checksum, payload3);
+
+    await writer.close();
+
+    const expected = Buffer.concat([
+      Buffer.from([0, 0, 0, 0, 0, 0, 0, 0, 0]),
+      payload1,
+      Buffer.from([0, 0, 0, 1, 0, 0, 0, 0, 0]),
+      payload2,
+      Buffer.from([0, 0, 0, 2, 0, 0, 0, 0, 0]),
+      payload3,
+    ]);
+
+    const content = readFileSync(filePath);
+    expect(content).toEqual(expected);
+  });
+
+  it("Write large number of entries.", async () => {
+    const randomName = Math.random().toString(36).substring(7);
+    const filePath = path.join(__dirname, `${randomName}.wal`);
+    files.push(filePath);
+    const file = await open(filePath, "a+");
+    const writer = new SegmentWriter(file.createWriteStream());
+    const typ = 0;
+    const checksum = 0;
+
+    for (let i = 0; i < 1000; i++) {
+      await writer.write(i, typ, checksum, Buffer.from("test" + i));
+    }
+
+    await writer.sync();
+
+    const content = readFileSync(filePath, {
+      flag: "r",
+    });
+    const expected = Buffer.concat(
+      Array.from({ length: 1000 }, (_, i) => {
+        const header = Buffer.alloc(9);
+
+        header.writeUInt32BE(i, 0);
+        header.writeUInt8(typ, 4);
+        header.writeUInt32BE(checksum, 5);
+        return Buffer.concat([header, Buffer.from("test" + i)]);
+      })
+    );
+
+    // writeFileSync(path.join(__dirname, "expected.wal"), expected);
+    // const expectedFile = readFileSync(path.join(__dirname, "expected.wal"));
+
+    // for (let i = 0; i < expectedFile.length; i++) {
+    //   if (expectedFile[i] !== content[i]) {
+    //     console.log(expectedFile.length);
+    //     console.log(content.length);
+    //     console.log(content.subarray(0, 100));
+    //     console.log(expectedFile.subarray(0, 100));
+    //     console.log(writer.size);
+
+    //     console.log("Mismatch at byte", i);
+    //     console.log("Expected:", expectedFile[i]);
+    //     console.log("Actual:", content[i]);
+    //     break;
+    //   }
+    // }
+
+    // Compare hashes instead of buffers
+    const fileHash = createHash("sha1").update(content).digest("hex");
+    const expectedHash = createHash("sha1").update(expected).digest("hex");
+    expect(fileHash).toBe(expectedHash);
+  });
+});
