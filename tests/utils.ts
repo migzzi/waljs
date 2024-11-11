0db33734644f30b5eeeef5e3ace8240b67f9942a
maged <m.magdy@awaed.co>
2024-11-11 20:14:26 +0200

    chore: Add tests utils.

---
+++ tests/utils.ts
@@ -0,0 +1,37 @@
+import { IEntry, Reader } from "../lib/entry";
+
+export class TextEntry implements IEntry {
+  public length: number;
+  public content: string;
+
+  static from(content: string): TextEntry {
+    const entry = new TextEntry();
+    entry.length = content.length;
+    entry.content = content;
+
+    return entry;
+  }
+
+  type(): number {
+    return 0;
+  }
+
+  encode(): Buffer {
+    const length = Buffer.alloc(4);
+    length.writeUInt32BE(this.length);
+
+    return Buffer.concat([length, Buffer.from(this.content)]);
+  }
+
+  decode(buf: Buffer): void {
+    this.length = buf.readUInt32BE(0);
+    this.content = buf.subarray(4, 4 + this.length).toString();
+  }
+
+  async read(r: Reader): Promise<Buffer> {
+    const length = await r.read(4);
+    const content = await r.read(length.readUInt32BE(0));
+
+    return Buffer.concat([length, content]);
+  }
+}
